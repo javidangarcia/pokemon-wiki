f454f85342f8fabb0c181fd3248051a2e6fa8a1b
Javier Garcia <javierdangarcia@techexchange.in>
2023-02-21 17:40:33 +0000

    added upload image to cloud backend feature

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,11 +1,10 @@
 # TODO(Project 1): Implement Backend according to the requirements.
 from google.cloud import storage
+
 class Backend:
 
     def __init__(self):
         self.client = storage.Client()
-        
-        
         
     def get_wiki_page(self, name, bucket_name):
         pass
@@ -13,12 +12,11 @@ class Backend:
     def get_all_page_names(self):
         pass
 
-    def upload(self,file_to_upload,bucket_name):
-        bucket = self.client.get_bucket(bucket_name)
-        object_to_be_stored = bucket.blob(file_to_upload.filename)
-        object_to_be_stored.upload_from_file( file_to_upload )
+    def upload(self, file, bucket):
+        bucket = self.client.get_bucket(bucket)
+        blob = bucket.blob('pokemon/' + file.filename)
+        blob.upload_from_file(file)
         
-
     def sign_up(self):
         pass
 
