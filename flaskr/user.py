f1d29fc1da6662ea76276e43ddf2380026442bd9
marktoro <marktoro@techexchange.in>
2023-02-22 01:59:07 +0000

    Added guid generator for user class

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,8 +1,8 @@
 from flask_login import UserMixin
+import uuid
 
 class User(UserMixin):
     def __init__(self, username, password):
-        self.id = username  # Temp
+        self.id = uuid.uuid4()
         self.username = username
         self.password = password
-    
