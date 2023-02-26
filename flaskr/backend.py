c74986d2f1df6ebc64f357c5d0430677885f928f
Javier Garcia <javierdangarcia@techexchange.in>
2023-02-26 19:37:22 +0000

    changed directories for upload backend method

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -13,35 +13,40 @@ class Backend:
     def get_wiki_page(self, name):
         bucket = self.client.get_bucket('wiki-content-techx')
         blob = bucket.get_blob(f'pages/{name}')
+
+        # reading json object blob and returning its contents
         with blob.open('r') as f:
             content = f.read()
         return content
 
     def get_all_page_names(self):
         bucket = self.client.get_bucket('wiki-content-techx')
-        blobs = bucket.list_blobs(prefix = 'pokemon/')
+        blobs = bucket.list_blobs(prefix = 'pages/')
         page_names = []
 
         # adding every blob to page_names except the first blob since it's just the folder name
         for index, blob in enumerate(blobs):
-            if index != 0:
-                page_names.append(blob.name)
+            if index == 0: continue
+            page_names.append(blob.name)
         return page_names
 
-    def upload(self, file, pokemon_dict ):
+    def upload(self, file, pokemon_dict):
         bucket = self.client.get_bucket('wiki-content-techx')
     
-        # image upload
-        img_blob = bucket.blob('poke_imgs/' + file.filename)
-        img_blob.upload_from_file(file)
+        # uploading user image of pokemon to the pokemon blob
+        pokemon = bucket.blob(f'pokemon/{file.filename}')
+        pokemon.upload_from_file(file)
 
-        # add image url to pokemon dictionary and turn into json object
+        # adding image url to pokemon dictionary
         img_url = AUTHENTICATED_URL + file.filename
         pokemon_dict["img_url"] = img_url
-        json_obj = json.dumps( pokemon_dict )
 
-        # json object upload
-        json_blob = bucket.blob('pokemon/' + pokemon_dict["name"])
+        # converting pokemon dictionary to json object
+        json_obj = json.dumps(pokemon_dict)
+
+        # creating a json object blob in the pages blob
+        name = pokemon_dict["name"].lower()
+        json_blob = bucket.blob(f'pages/{name}')
         json_blob.upload_from_string(data=json_obj, content_type="application/json")
         
     def sign_up(self, username, password):
@@ -95,6 +100,3 @@ class Backend:
 # backend.sign_up('javiergarcia', 'pokemon123')
 # backend.sign_in('javiergarcia', 'poke525') # should return False because it doesn't match password in cloud storage
 # backend.sign_in('javiergarcia', 'pokemon123') # should return True and sign the user in
-
-backend = Backend()
-backend.sign_up('javiergarc', '12')
