a60ca2f59f2e087f3179c09d1059dc4b269def26
marktoro <marktoro@techexchange.in>
2023-03-02 01:33:01 +0000

    Changed user id to be equal to username.

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,8 +1,7 @@
 from flask_login import UserMixin
-import uuid
 
 class User(UserMixin):
     def __init__(self, username, password):
-        self.id = uuid.uuid4()
+        self.id = username
         self.username = username
         self.password = password
