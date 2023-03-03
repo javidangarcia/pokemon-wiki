7c4b5c6a1b5110dbae0fa7367f81a17b3101280f
marktoro <marktoro@techexchange.in>
2023-03-03 01:53:42 +0000

    Added username to nav bar and message, restricted some links

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -61,7 +61,7 @@ def make_endpoints(app):
             if login_:
                 user = User(form.username.data, form.password.data)
                 flask_login.login_user(user)
-                flash('Logged in succesfully.')
+                flash(f'Welcome {flask_login.current_user.username.upper()}!')
 
                 return redirect(url_for('home'))
             else:
