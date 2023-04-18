c2813b086168406815d13932efcb370da61108f7
Edgar Ochoa Sotelo <edgarochoa@techexchange.in>
2023-04-18 01:17:47 +0000

    pages tests finished

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -44,9 +44,6 @@ def fake_file():
 def mock_rand():
     return MagicMock()
 
-
-# TODO(Checkpoint (groups of 4 only) Requirement 4): Change test to
-# match the changes made in the other Checkpoint Requirements.
 @patch("flaskr.backend.Backend.get_image",
        return_value=b"This should have been a real image!")
 def test_home_page(mock_get_wiki_page, client):
@@ -86,8 +83,6 @@ def test_upload_get(client):
 
 
 # should return page for abra
-
-
 @patch("flaskr.backend.Backend.get_wiki_page", return_value=b"{'name':'diff'}")
 @patch("flask.json.loads",
        return_value={
@@ -103,7 +98,6 @@ def test_get_wiki_page(mock_json, mock_get_page, client):
     response = client.get("/pages/abra")
     assert b"abra" in response.data
     mock_json.assert_called_once_with(b"{'name':'diff'}")
-
 
 
 # Tests sign up page
@@ -135,7 +129,6 @@ def test_logout(client):
        return_value=b"Uploaded a test pokemon!")
 @patch("flaskr.backend.Backend.get_all_page_names",
        return_value=["name1", "name2", "name3"])
-#@patch("flask.request.files",return_value=fake_file)
 def test_upload_post(mock_get_all_pages, mock_upload, app, client):
     with app.test_request_context("",
                                   query_string={
@@ -147,7 +140,6 @@ def test_upload_post(mock_get_all_pages, mock_upload, app, client):
                                       'image-name': '',
                                       'image-type': ''
                                   }):
-
         response = client.post("/upload")
         #assert response.status_code == 0
         assert request.args.get("name") == "abra"
@@ -155,31 +147,30 @@ def test_upload_post(mock_get_all_pages, mock_upload, app, client):
         assert mock_get_all_pages() == ["name1", "name2", "name3"]
 
 
+def test_game(client,app):
+    with app.test_request_context("",
+                                query_string={
+                                    'name': 'abra',
+                                    'image': 'asdfEFDDSEDFEE',
+                                }):
+        response = client.get("/game")
+        assert request.args.get("name") == "abra"
+        assert request.args.get("image") == "asdfEFDDSEDFEE"           
 
 
+def test_game_post(app, client):
+    with app.test_request_context("",
+                                  query_string={
+                                      'name': 'abra',
+                                      'rank': '5',
+                                      'user': 'user1',
+                                      'nature': '',
+                                      'level': '',
+                                      'image-name': '',
+                                      'image-type': ''
+                                  }):
+        response = client.post("/game")
+        assert request.args.get("name") == "abra"
+        assert request.args.get("rank") == "5"
+        assert request.args.get("user") == "user1"
 
-"""
-    @app.route("/game")
-    @flask_login.login_required
-    def play_game(pokemon_id=1):
-        # make sure pokemon_id has not been guessed before
-        seen = backend.get_seen_pokemon(flask_login.current_user.username)
-        pokemon_id = randbelow(810)
-        while str(pokemon_id) in seen:
-            pokemon_id = randbelow(810)
-
-        # check that image is not a None type
-        pokemon_img = None
-        while pokemon_img == None:
-            pokemon_img = backend.get_pokemon_image(pokemon_id)
-
-        # Get the pokemon and user data
-        pokemon_data = backend.get_pokemon_data(pokemon_id)
-        user = backend.get_game_user(flask_login.current_user.username)
-        pokeball_img = backend.get_pokeball()
-        answer = pokemon_data['name']['english']
-
-        # return template
-        return render_template("game.html",image=pokemon_img,data=pokemon_data,user=user,pokeball=pokeball_img,answer=answer,seen=seen)
-
-"""
